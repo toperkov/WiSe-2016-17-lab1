98ea2d6ced9464e511e841fac9f42228d46f010c
Toni Perkovic <toperkov@gmail.com>
2017-03-01 22:11:03 +0100

    Update SerialMonitor.ino

--- examples/SerialMonitor.ino
+++ examples/SerialMonitor.ino
@@ -1,1 +1,16 @@
+/*
+ * This is the Hello World! for Arduino. 
+ * It shows how to send data to the computer via Serial port
+ */
 
+
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+}
+
+void loop()                       // run over and over again
+{
+  Serial.println("Hello world!");  // prints hello with ending line break
+  delay(1000);
+}
